78d95c70164d622a6ee3cee5826bd40ce3d826ac
Kate F <follett.katie@gmail.com>
2019-06-26 16:27:50 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+// Create a variable called multiply that will multiply var1 and var2 whose results will equal "62".
+//Create a variable called random that will generate a random integer greater than 0.
+//Create a variable called mod set to an equation whose remainder is equal to "4".
+//Create a variable called max that will return "20" as the highest number in the set.
+
+const newID = 1000000000 + oldID;
+
+const ageIsValid = Number.isInteger(currentAge);
+
+const randomNumber = Math.random() * 20;
+
+const randomInteger = Math.floor(randomNumber);
+
+const randomUserID = randomInteger + 1000000001;
+
