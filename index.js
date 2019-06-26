867fb5ba12871422874a72f7cab16c7765b35eb5
Kate F <follett.katie@gmail.com>
2019-06-26 16:26:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+// Create a variable called multiply that will multiply var1 and var2 whose results will equal "62".
+//Create a variable called random that will generate a random integer greater than 0.
+//Create a variable called mod set to an equation whose remainder is equal to "4".
+//Create a variable called max that will return "20" as the highest number in the set.
+
+let var1 = 31;
+let var2 = 2;
+
+
+
+let multiply = '62';
+
